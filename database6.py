d8ceed423a14f7cb953f2ae670eb74cb19df902a
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-10-10 14:54:28 +0530

    Delete A Row From student Table Using eno

---
+++ database6.py
@@ -0,0 +1,62 @@
+# Python Program To Delete A Row From student Table By Accepting The Student eno number
+
+'''
+Function Name    :  Delete A Row From student Table Using eno
+Function Date    :  10 Oct 2020
+Function Author  :  Prasad Dangare
+Input            :  String
+Output           :  String
+'''
+
+import MySQLdb
+
+# Function To Store Row Into The Student Table
+
+def delete_rows(eno):
+    
+    # Connect To MySQL DataBase
+    
+    conn = MySQLdb.connect(host='localhost', database="pythondb", user='prasadD', password='prasadD@93')
+
+    # Prepare A Cursor Object Using Cursor() Method
+
+    cursor = conn.cursor()
+    
+    # Prepare SQL Query String To Delete A Row
+    
+    str = "delete from student where eno = '%d' " 
+    
+    # Define The Arguments
+    
+    args = (eno)
+    try:
+        
+        # Execute The SQL Query Using execute() method
+        
+        cursor.execute(str % args)
+        
+        # Save The Changes To The DataBase
+        
+        conn.commit()
+        print('1 Row Deleted...')
+        
+    except:
+        
+        # RollBack If There Is Any Problem
+        
+        conn.rollback()
+        
+    finally:
+        
+        # Close Connection
+        
+        cursor.close()
+        conn.close()
+        
+# Enter Student Number Whose Row Is To Be Deleted
+
+x = int(input("Enter eno : "))
+
+# Pass eno To delete_rows() Function
+
+delete_rows(x)
