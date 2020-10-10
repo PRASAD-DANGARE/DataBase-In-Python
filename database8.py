a930072a439bfdbcb32200bcdb842cb3fc364286
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-10-10 14:55:44 +0530

    Create Emptab In MYSQL DataBase

---
+++ database8.py
@@ -0,0 +1,39 @@
+# Python Program To Create Emptab In MYSQL DataBase
+
+'''
+Function Name    :  Create Emptab In MYSQL DataBase
+Function Date    :  10 Oct 2020
+Function Author  :  Prasad Dangare
+Input            :  String
+Output           :  String
+'''
+
+import MySQLdb
+
+# Connect To MySQL DataBase
+
+conn = MySQLdb.connect(host='localhost', database="pythondb", user='prasadD', password='prasadD@93')
+
+# Prepare A Cursor Object Using Cursor() Method
+
+cursor = conn.cursor()
+
+# Delete Table If Already Exists
+
+cursor.execute("drop table if exists emptab")
+
+# Prepare SQL String To Create A New Table
+
+str = "create table emptab(eno int, ename char(20), sex char(1), salary float)"
+
+# Execute The Query To Create The Table
+
+cursor.execute(str)
+print('Table Created...')
+
+# Close Connection
+
+cursor.close()
+conn.close()
+
+    
