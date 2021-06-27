5ada4a646995f3530667492bafcd62138e5f621b
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2021-06-27 20:32:14 +0530

    Delete A Record From emptab

---
+++ DataBase4.py
@@ -0,0 +1,49 @@
+'''
+Function Name    :  Delete A Record From emptab
+Function Date    :  27 June 2021
+Function Author  :  Prasad Dangare
+Input            :  Int
+Output           :  Record Get Deleted From The Row
+
+'''
+
+# Delete a row from table
+
+import MySQLdb
+
+def Delete_Rows(eno):
+
+    Connction = MySQLdb.connect(host = "localhost", database = "pythondb", user = "root", password = "prasadD@93")
+
+    cobj = Connction.cursor()
+
+    str = "delete from emptab where eno = '%d' "
+
+    value = (eno)
+
+    try:
+
+        cobj.execute(str % value)
+        Connction.commit()
+        
+        print("Row Deleted...")
+
+    except:
+
+        Connction.rollback()
+        print("Not Deleted")
+
+    finally:
+
+        cobj.close()
+        Connction.close()
+
+def main():
+
+    iNo = int(input("Enter The Eno : "))
+
+    Delete_Rows(iNo)
+
+if __name__ == "__main__":
+    main()
+
